adce30ca0c692da897d1d073b4527471afc0da67
xiaokk277 <botono123@gmail.com>
2024-03-14 21:54:46 +0700

    nav bar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+import { IoMdSearch } from "react-icons/io";
+import {FaCartShopping} from "react-icons/fa6";
+
+const Menu = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Top Rated",
+        link: "/#services",
+    },
+    {
+        id: 3,
+        name: "Kids Wear",
+        link: "/#",
+    },
+    {
+        id: 4,
+        name: "Men Wear",
+        link: "/#",
+    },
+];
+
+
+const Navbar = () => {
+    return (
+        <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+            {/* uppper navbar */}
+            <div className="bg-primary/40 py-2">
+                <div className="container flex justify-between items-center">
+                    <div>
+                        <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+                            <img src={Logo} alt="logo" className="w-10 "/>TimmyFashion
+                        </a>
+                    </div>
+                    <div className="flex justify-between items-center gap-4">
+                        <div className="relative group hidden sm:block">
+                            <input type="text" placeholder="Search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"/>
+                            <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3"
+                            />
+                        </div>
+                        {/* order button */}
+                        <button onClick={()=>alert("Ordering not available yet")} className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
+                        >
+                            <span className="group-hover:block hidden transition-all duration-200">Order</span>
+                            <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer"/>
+                        </button>
+                        {/* Darkmode switch */}
+                        <div>
+                            {/* <DarkMode /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* lower navbar */}
+            <div className="flex justify-center">
+                <ul className="sm:flex hidden items-center gap-4">
+                    {Menu.map((data) => (
+                        <li key={data.id}>
+                            <a href={data.link} className="inl" >{data.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
