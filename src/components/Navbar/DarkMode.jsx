ef3b17ccec8dcafd2cc466eedf26f3efa6bcbb48
xiaokk277 <botono123@gmail.com>
2024-03-15 03:31:33 +0700

    add products

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { MdDarkMode } from "react-icons/md";
-import { MdLightMode } from "react-icons/md";
+import LightButton from "../../assets/websites/light-mode-button.png"
+import DarkButton from "../../assets/websites/dark-mode-button.png"
 
 
 const DarkMode = () => {
     const [theme, setTheme] = React.useState(
-        localStorage.getItem("theme")? localStorage.getItem("theme") : "light"
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
     )
     const element = document.documentElement;
+    
     React.useEffect(()=>{
         if(theme === "dark"){
             element.classList.add("dark");
@@ -17,14 +18,15 @@ const DarkMode = () => {
             element.classList.remove("dark");
             localStorage.setItem("theme", "light")
         } 
-    },[theme]);
+    } , [theme, element.classList]);
     
     return (
         <div className='relative'>
-            <img src= {MdLightMode} alt="" onClick={()=> setTheme(theme=== "light" ? "dark" : "light")} className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme==="dark" ? "opacity-0": "opacity-100"}`} />
-            <img src= {MdDarkMode} alt="" onClick={()=> setTheme(theme=== "light" ? "dark" : "light")} className='w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 '/>
+            <img src= {LightButton} alt="" onClick={() => setTheme(theme === "light" ? "dark" : "light")} className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : "opacity-100"} `} />
+
+            <img src= {DarkButton} alt="" onClick={() => setTheme(theme === "light" ? "dark" : "light")} className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 "/>
         </div>
     );
-}
+};
 
 export default DarkMode;
