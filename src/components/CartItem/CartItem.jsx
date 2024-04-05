45000d1d298d56e641e89c6f302357444819fe3a
xiaokk277 <botono123@gmail.com>
2024-04-05 16:53:51 +0700

    complete ver 1

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { ShopContext } from '../../Context/ShopContext';
 import { useContext } from 'react';
 import { IoBagRemoveSharp } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
 const CartItem = () => {
-    const {getTotalCartAmount, ProductsData, cartItem, removeFromCart} = useContext(ShopContext)
+    const {getTotalCartAmount, ProductsData, cartItem, removeFromCart, addToCart} = useContext(ShopContext);
+    
     return (
         <div className='container flex flex-col my-3'>
             <div className='grid grid-cols-9 items-center font-bold'>
@@ -23,7 +26,11 @@ const CartItem = () => {
                                 <img src={e.img} alt="" className='h-[100px] w-[100px] col-span-2'/>
                                 <p className='col-span-2'>{e.name}</p>
                                 <p className='col-span-1'>${e.new_price}.00</p>
-                                <button type='number' className='col-span-1'>{cartItem[e.id]}</button>
+                                <div className='flex flex-row items-center gap-4'>
+                                    <FaMinus onClick={() => removeFromCart(e.id)} className='col-span-1'/>
+                                    <span className='col-span-1'>{cartItem[e.id]}</span>
+                                    <FaPlus onClick={() => addToCart(e.id)} className='col-span-1'/>
+                                </div>
                                 <p className='col-span-1'>${e.new_price * cartItem[e.id]}.00</p>
                                 <IoBagRemoveSharp onClick={() => removeFromCart(e.id)} className='col-span-1'/>
                             </div>
